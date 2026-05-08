8c6c773e3cf402b033fc2ec9fad54b64a7819ad5
Devbezos <devbezos1994@gmail.com>
2026-05-07 22:46:06 -0400

    Disable craft warnings

--- features/services/LariasWeeklyChecklist_CraftingWarning.lua
+++ features/services/LariasWeeklyChecklist_CraftingWarning.lua
@@ -310,8 +310,7 @@ end
 -- shows or hides the warning panel accordingly.
 function Addon:CheckCraftingWarning()
     if _warn then _warn.holder:Hide() end
-    return -- crafting warning globally disabled
-    if self:EnsurePrefs().craftWarnDisabled then return end
+    do return end -- crafting warning globally disabled
 
     local itemID, itemLink
     if _pendingItemID and _pendingItemID > 0 then
